34d5245f940c0d95adaa1edfcef0fc76451bf050
louiscarterjr29@gmail.com <louiscarterjr29@gmail.com>
2019-05-15 17:54:47 -0400

    Changes to firebase add

---
+++ src/app/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAO6MG1hZYXTnmemo8m9OuIYoaELO5cH1A",
+    authDomain: "ppvip-240522.firebaseapp.com",
+    databaseURL: "https://ppvip-240522.firebaseio.com",
+    projectId: "ppvip-240522",
+    storageBucket: "ppvip-240522.appspot.com",
+    messagingSenderId: "670482589586",
+    appId: "1:670482589586:web:544bcf6c46481a08"
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
